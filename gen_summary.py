97b30a24eef137cc85d3edc4a822005d696558c6
michelleblom <michelle.blom@gmail.com>
2022-11-30 11:56:22 +1100

    Added script for generating CSV summaries of more complex STV data files, used for analysis in other repos

---
+++ gen_summary.py
@@ -0,0 +1,30 @@
+import argparse
+import os
+import numpy as np
+import sys
+
+from utils import read_ballots_json, simulate_stv, print_summary
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    # Input: stv data file
+    parser.add_argument('-d', dest='data')
+
+    # Input: number of seats in election (default is 2)
+    parser.add_argument('-seats', dest='seats', default=2, type=int)
+
+    args = parser.parse_args()
+
+    candidates, ballots, id2group, cid2num, valid_ballots = \
+        None, None, None, None, None
+
+    candidates, ballots, id2group, cid2num, valid_ballots = \
+        read_ballots_json(args.data)
+
+    order_c = []
+    order_a = []
+
+    quota = simulate_stv(ballots, candidates, args.seats, order_c, order_a) 
+
+    print_summary(candidates, id2group, args.seats, quota, order_c, order_a)
---
+++ summarise.sh
@@ -0,0 +1,11 @@
+
+python gen_summary.py -d data/FedAus22/FederalSenate2022NSW.json -seats 6 > NSW22.csv
+
+python gen_summary.py -d data/FedAus22/2022ACT.json -seats 2 > ACT22.csv
+python gen_summary.py -d data/FedAus22/2022NT.json -seats 2 > NT22.csv
+python gen_summary.py -d data/FedAus22/FederalSenate2022WA.json -seats 6 > WA22.csv
+python gen_summary.py -d data/FedAus22/FederalSenate2022VIC.json -seats 6 > VIC22.csv
+python gen_summary.py -d data/FedAus22/FederalSenate2022TAS.json -seats 6 > TAS22.csv
+python gen_summary.py -d data/FedAus22/FederalSenate2022SA.json -seats 6 > SA22.csv
+python gen_summary.py -d data/FedAus22/FederalSenate2022QLD.json -seats 6 > QLD22.csv
+
