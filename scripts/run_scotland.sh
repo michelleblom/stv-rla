cc58f6ae23ebb1e9e7b4fb4fb9e5dea52e52836d
michelleblom <michelle.blom@gmail.com>
2023-07-19 19:50:17 +1000

    Add results of scripts for current state of audit_general.py

--- scripts/run_scotland.sh
+++ scripts/run_scotland.sh
@@ -21,6 +21,6 @@ for instance in data/Scotland/*.txt; do
     rl=0.10
     er=0.002
     
-    python3 audit_general.py -d ${instance} -outcome ${bn}.otc_D2 -quota ${quota} -voters ${voters} -e $er -r $rl -log "${bn}.${rl}.${er}.log"
+    python3 audit_general.py -d ${instance} -outcome ${bn}.otc_D2 -quota ${quota} -voters ${voters} -e1 $er -e2 0 -r $rl -log "${bn}.${rl}.${er}.log"
 done
 
