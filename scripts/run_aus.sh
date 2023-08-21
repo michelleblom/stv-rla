29c92cad249095f199c4ac2c2bab58786dcd49f2
michelleblom <michelle.blom@gmail.com>
2023-08-21 17:43:21 +1000

    Reran scripts; adjusted while condition in outer loop of new 1 quota method

--- scripts/run_aus.sh
+++ scripts/run_aus.sh
@@ -20,11 +20,9 @@ python3.9 audit_general.py -d data/FedAus/federal_2019_ACT.stv -outcome data/Fed
 
 python3.9 audit_general.py -d data/FedAus22/2022ACT.json -outcome data/FedAus22/2022ACT.otc_D2 -quota 90078 -voters 276651 -e1 0.002 -e2 0 -r 0.10 -log "data/FedAus/federal_2022_ACT.0.10.0.002.log" 
 
-python3.9 audit_general.py -d data/FedAus/federal_2016_NT.stv -outcome data/FedAus/federal_2016_NT.otc -quota 34010 -voters 105539 -e1 0.002 -e2 0 -r 0.10 -log "data/FedAus/federal_2016_NT.0.10.0.002GEN.log" 
-
-python3.9 audit_general.py -d data/FedAus/federal_2019_NT.stv -outcome data/FedAus/federal_2019_NT.otc -quota 35010 -voters 108994 -e1 0.002 -e2 0 -r 0.10 -log "data/FedAus/federal_2019_NT.0.10.0.002GEN.log" 
 
 
-python3.9 audit_general.py -d data/FedAus22/2022NT.json -outcome data/FedAus22/2022NT.otc_D2 -quota 35010 -voters 108994 -e1 0.002 -e2 0 -r 0.10 -log "data/FedAus/federal_2022_NT.0.10.0.002GEN.log" 
+python3.9 audit_general.py -d data/FedAus/federal_2016_NT.stv -outcome data/FedAus/federal_2016_NT.otc -quota 34010 -voters 102027 -e1 0.002 -e2 0 -r 0.10 -log "data/FedAus/federal_2016_NT.0.10.0.002.log" 
 
+python3.9 audit_general.py -d data/FedAus/federal_2019_NT.stv -outcome data/FedAus/federal_2019_NT.otc -quota 35010 -voters 105027 -e1 0.002 -e2 0 -r 0.10 -log "data/FedAus/federal_2019_NT.0.10.0.002.log" 
 
--- scripts/run_gen_test.sh
+++ scripts/run_gen_test.sh
@@ -1,6 +1,7 @@
 
 instances=(data/US/2014_OAK_MAYOR.us data/US/BK_CC_D8_2014.us data/US/test_Aspen09CC_ballots.us data/US/test_Pierce08CAS_ballots.us data/Ireland/DublinNorth2002_ballots.txt data/Ireland/DublinWest2002_ballots.txt data/Scotland/GCC_07_Anderson_ballots.txt data/Scotland/GCC_07_Baillieston_ballots.txt data/Scotland/GCC_07_Canal_ballots.txt data/Scotland/GCC_07_Craigton_ballots.txt data/Scotland/GCC_07_Drumchapel_ballots.txt data/Scotland/GCC_07_EastCentre_ballots.txt data/Scotland/GCC_07_Garscadden_ballots.txt data/Scotland/GCC_07_Govan_ballots.txt data/Scotland/GCC_07_GreaterPollock_ballots.txt data/Scotland/GCC_07_Hillhead_ballots.txt data/Scotland/GCC_07_Langside_ballots.txt data/Scotland/GCC_07_Linn_ballots.txt data/Scotland/GCC_07_Maryhill_ballots.txt data/Scotland/GCC_07_Newlands_ballots.txt data/Scotland/GCC_07_NorthEast_ballots.txt data/Scotland/GCC_07_Partick_ballots.txt data/Scotland/GCC_07_Pollockshields_ballots.txt data/Scotland/GCC_07_Shettleston_ballots.txt data/Scotland/GCC_07_SouthsideCentral_ballots.txt data/Scotland/GCC_07_Springburn_ballots.txt data/FedAus22/2022ACT.json data/FedAus22/2022NT.json)
 
+
 for instance in "${instances[@]}"; do 
     bn="${instance%%.*}"
     voters=`cat "${bn}.voters"`
