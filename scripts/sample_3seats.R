a66f9a5a68c4345708bcfbc21f019913308ea48d
michelleblom <michelleblom@users.noreply.github.com>
2025-01-22 12:42:10 +1100

    Deleted scripts

--- scripts/sample_3seats.R
+++
@@ -1,4 +0,0 @@
-a <- read.table("sample_3seats.tsv", header = TRUE)
-pdf("sample_3seats_boxplots.pdf")
-boxplot(ASN ~ Verified, a, main = "3 Seats", horizontal = TRUE, log = "x")
-dev.off()
--- scripts/sample_4seats.R
+++
@@ -1,4 +0,0 @@
-a <- read.table("sample_4seats.tsv", header = TRUE)
-pdf("sample_4seats_boxplots.pdf")
-boxplot(ASN ~ Verified, a, main = "4 Seats", horizontal = TRUE, log = "x")
-dev.off()
